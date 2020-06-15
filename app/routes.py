81872a5cba76776eb606315210f5f97f26078f95
ayanle <ayanle226@gmail.com>
2020-06-15 07:07:45 -0500

    Added all files

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,21 @@
 from app import app
-from flask import render_template,url_for
-
+from flask import render_template,url_for, flash, redirect
+from app.forms import LoginForm 
 
 
 @app.route('/about')
 def about():
 	return render_template('about.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-	return render_template('login.html', title='Login')
+	form = LoginForm()
+
+	if form.validate_on_submit():
+		flash("login successful", 'success')
+		return redirect('/index')
+
+	return render_template('login.html', title='Login', form=form)
 
 @app.route('/')
 @app.route('/index')
